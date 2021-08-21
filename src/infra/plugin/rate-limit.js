e40f06dd5c4e91400e08069950029567f9cd8d05
Yuri Fontella <yurifc4@gmail.com>
2021-08-21 03:31:05 -0300

    ncu

--- src/infra/plugin/rate-limit.js
+++ src/infra/plugin/rate-limit.js
@@ -4,8 +4,7 @@ const fp = require('fastify-plugin')
 
 module.exports = fp(async (app) => {
   app.register(require('fastify-rate-limit'), {
-    max: 50,
-    ban: 5,
+    max: 25,
     timeWindow: '24h',
     errorResponseBuilder: () => (
       { statusCode: 429, error: 'Muitos pedidos', message: 'Limite de taxa excedido' }
